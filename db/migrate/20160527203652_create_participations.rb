393ab523e6d0c28ca2fa175332ddd8a77e08f21c
preller <ole@hirnstrom.net>
2016-06-03 21:02:53 +0200

    added some more attributes to the participation model and added some more infos to fleet show

--- db/migrate/20160527203652_create_participations.rb
+++ db/migrate/20160527203652_create_participations.rb
@@ -3,13 +3,22 @@ class CreateParticipations < ActiveRecord::Migration
     create_table :participations do |t|
       t.references :fleet
       t.string :eve_charid
+      t.string :eve_charname
 
       t.string :eve_corpid
+      t.string :eve_corpname
+
       t.string :eve_allianceid
+      t.string :eve_alliancename
+
       t.string :eve_shiptypeid
+      t.string :eve_shiptypename
+
       t.string :eve_solarsystemid
       t.string :eve_stationid
+
       t.string :eve_constellationid
+      t.string :eve_constellationname
 
       t.timestamps null: false
     end

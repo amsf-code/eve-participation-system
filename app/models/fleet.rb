8a764fbce2a3d27e29333143eb1c56aa4ca4b82a
preller <ole@hirnstrom.net>
2016-06-03 19:50:40 +0200

    added time validation for participations

--- app/models/fleet.rb
+++ app/models/fleet.rb
@@ -4,4 +4,9 @@ class Fleet < ActiveRecord::Base
   def participating?(eve_charid)
     participations.find_by(eve_charid: eve_charid).present?
   end
+
+  def expired?
+    #expired if the fleet was created more than 20mins ago
+    created_at + (60 * 20) < Time.now
+  end
 end

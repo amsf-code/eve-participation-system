fef14e0c354f2ab3a391e1cf07399fcb0a58824d
Isoterik <isoterik@gmx.net>
2016-06-03 23:32:49 +0200

    added fleet form validation #4

--- app/controllers/fleet_controller.rb
+++ app/controllers/fleet_controller.rb
@@ -1,9 +1,25 @@
 class FleetController < ApplicationController
   def create
     fleet = Fleet.new(fleet_params)
-    fleet.save
 
-    redirect_to fleet_path(fleet)
+    if fleet.name == ''
+      flash[:fleet_name_missing] = 'Fleet name is missing!'
+      redirect_to root_path
+    elsif fleet.fc_name == ''
+      flash[:fleet_commander_name_missing] = 'Fleet commander name is missing!'
+      redirect_to root_path
+    elsif fleet.doctrin == ''
+      flash[:doctrin_missing] = 'Doctrin in use is missing!'
+      redirect_to root_path
+    elsif fleet.details == ''
+      flash[:details_missing] = 'Details are missing!'
+      redirect_to root_path
+    elsif fleet.duration == ''
+      flash[:duration_missing] = 'Duration is missing!'
+      redirect_to root_path
+    elsif fleet.save
+      redirect_to fleet_path(fleet)
+    end
   end
 
   def show

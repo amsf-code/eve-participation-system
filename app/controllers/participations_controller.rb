addba6464ed15e9d81a3699d208d316723351d15
preller <ole@hirnstrom.net>
2016-06-08 21:21:31 +0200

    fixed some offenses aka bugs

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -6,15 +6,15 @@ class ParticipationsController < ApplicationController
   end
 
   def new
-    @participating = @fleet.participating?(eve_char_info.id)
+    @participating = @fleet.participating?(eve_character_info.id)
     @expired = @fleet.expired?
   end
 
   def create
-    if @fleet.participating?(eve_char_info.id) || @fleet.expired?
+    if @fleet.participating?(eve_character_info.id) || @fleet.expired?
       render :show
     else
-      @fleet.participations << Participation.build_from_eve_headers(eve_char_info)
+      @fleet.participations << Participation.build_from_eve_headers(eve_character_info)
       redirect_to fleet_participation_path(@fleet)
     end
   end

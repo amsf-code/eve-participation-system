75a42f9ece0f9b72c8745fbefd2784b1197c29d2
Jan-Oliver Jahner <jan-oliver.jahner@xing.com>
2016-06-08 15:15:54 +0200

    add rspec and factory_girl

--- test/controllers/fleet_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-require 'test_helper'
-
-class FleetControllerTest < ActionController::TestCase
-  test 'should get create' do
-    get :create
-    assert_response :success
-  end
-end

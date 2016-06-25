0e04f806faeb15030a31b4572b1c308975984f04
Isoterik <isoterik@gmx.net>
2016-06-25 18:17:13 +0200

    added session user token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = user.id
+    session[:user_token] = auth_hash['credentials']['token']
     redirect_to user
   end
 

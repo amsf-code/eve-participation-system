689702c75f89b94aa182c4670d041a7e308148b4
Isoterik <isoterik@gmx.net>
2016-06-29 17:34:35 +0200

    added some more informations to fleet show

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class UserController < ApplicationController
   load_and_authorize_resource
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 module ApplicationHelper
-  def ship_image_tag(ship_type_id)
-    image_tag 'https://image.eveonline.com/Render/' + ship_type_id.to_s + '_128.png'
+  def ship_image_tag(ship_id)
+    image_tag 'https://image.eveonline.com/Render/' + ship_id + '_128.png'
   end
 
   def char_image_tag(char_id)
-    image_tag 'https://image.eveonline.com/Character/' + char_id.to_s + '_128.jpg'
+    image_tag 'https://image.eveonline.com/Character/' + char_id + '_128.jpg'
   end
 
   def corp_image_tag(corp_id)
-    image_tag 'https://image.eveonline.com/Corporation/' + corp_id.to_s + '_128.png'
+    image_tag 'https://image.eveonline.com/Corporation/' + corp_id + '_128.png'
+  end
+
+  def system_link_to(system_name)
+    link_to("#{system_name}", 'http://evemaps.dotlan.net/system/' + system_name)
   end
 end

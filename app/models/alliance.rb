9ddc4c9d40a627cf49abc9975069d1e0a5223952
Isoterik <isoterik@gmx.net>
2016-06-30 17:18:12 +0200

    swapped the fleet participation details to participations/show.html.erb from fleet/show.html.erb

--- app/models/alliance.rb
+++ app/models/alliance.rb
@@ -1,4 +1,5 @@
 class Alliance < ActiveRecord::Base
   has_many :corporations
   has_many :fleets
+  has_many :characters, through: :corporations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   resources :fleet, only: %i(create show new) do
-    resource :participation, only: %i(create new show)
+    resource :participation, only: %i(create show)
   end
 
   resources :user, only: %i(show)

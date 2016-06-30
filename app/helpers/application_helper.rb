01d7ebb7f4f5002b917539313b09707534ec06ad
Isoterik <isoterik@gmx.net>
2016-06-30 05:38:45 +0200

    some gui alterations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 module ApplicationHelper
   def ship_image_tag(ship_id)
-    image_tag 'https://image.eveonline.com/Render/' + ship_id + '_128.png'
+    image_tag 'https://image.eveonline.com/Render/' + ship_id + '_64.png'
   end
 
   def char_image_tag(char_id)
-    image_tag 'https://image.eveonline.com/Character/' + char_id + '_128.jpg'
+    image_tag 'https://image.eveonline.com/Character/' + char_id + '_64.jpg'
   end
 
   def corp_image_tag(corp_id)
-    image_tag 'https://image.eveonline.com/Corporation/' + corp_id + '_128.png'
+    image_tag 'https://image.eveonline.com/Corporation/' + corp_id + '_64.png'
   end
 
   def system_link_to(system_name)
